9b58fe3da831153509bfa0d8284bf32003cc8344
carolinabras <carolina.bras@tecnico.ulisboa.pt>
2023-10-16 00:34:23 +0100

    attack and health

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int maxHealth = 200; // max health of enemy
+    public int currentHealth; // current health of enemy
+    public int basicDamage = 40; // basic damage of enemy
+    [SerializeField] private GameObject player; // player game object
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth; // set current health to max health
+       
+        
+    }
+    
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage; // subtract damage from current health
+        if (currentHealth <= 0) // if current health is less than or equal to 0
+        {
+            Die(); // die
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerMovement>().TakeDamage(basicDamage);
+            
+        }
+    }
+    
+    
+
+    void Die()
+    {
+        //can create a dying animation if you have it and then destroy the enemy
+        Debug.Log("Enemy died!"); // log that enemy died
+        Destroy(gameObject); // destroy enemy
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -19,7 +20,8 @@ public class PlayerMovement : MonoBehaviour
     
     private enum MovementState { idle, running, jumping, falling } // movement states of player
     
-    
+    public int maxHealth = 200; // max health of player
+    public int currentHealth; // current health of player
     
     // Start is called before the first frame update
     void Start()
@@ -29,6 +31,7 @@ public class PlayerMovement : MonoBehaviour
         anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
         collider = GetComponent<BoxCollider2D>();
+        currentHealth = maxHealth; // set current health to max health
     }
 
     // Update is called once per frame
@@ -87,5 +90,25 @@ public class PlayerMovement : MonoBehaviour
     private bool isGrounded()
     {
        return Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0f, Vector2.down, .1f, jumpableGround); // creates a new box (like the box collider) and checks if it is overlaping with the terrain
+    }
+    
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage; // subtract damage from current health
+        if (currentHealth <= 0) // if current health is less than or equal to 0
+        {
+            Die(); // die
+        }
+    }
+    
+    void Die()
+    {
+        //wait for 2 seconds
+        //restart the game
+        //can create a dying animation if you have it and then destroy the enemy
+        Debug.Log("Player died!"); // log that enemy died
+        Destroy(gameObject); // destroy enemy
+        SceneManager.LoadScene(0);
+
     }
 }
